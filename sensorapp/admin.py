adbed0b06d654f8345c266c7f9c159fb404a688f
Xabi85 <xabi_ota@hotmail.com>
2024-05-31 16:53:48 +0200

    incorporar admin

--- sensorapp/admin.py
+++ sensorapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import LecturaTemperatura, LecturaTemperatura2, EstadoTurbinas
 
-# Register your models here.
+# Registro de LecturaTemperatura
+@admin.register(LecturaTemperatura)
+class LecturaTemperaturaAdmin(admin.ModelAdmin):
+    list_display = ('fecha', 'temperatura')
+    list_filter = ('fecha',)
+    search_fields = ('fecha', 'temperatura')
+
+# Registro de LecturaTemperatura2
+@admin.register(LecturaTemperatura2)
+class LecturaTemperatura2Admin(admin.ModelAdmin):
+    list_display = ('fecha', 'temperatura')
+    list_filter = ('fecha',)
+    search_fields = ('fecha', 'temperatura')
